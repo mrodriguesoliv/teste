ffd121a84392361a20a24b429096bb38436cc0ff
mrodriguesoliv <168552816+mrodriguesoliv@users.noreply.github.com>
2024-05-16 00:28:10 +0000

    15.08 21:28

--- projetoblog/blog/views.py
+++ projetoblog/blog/views.py
@@ -4,11 +4,13 @@ from django.db.models import Q
 from django.db.models.query import QuerySet
 from django.shortcuts import render
 from django.views.generic import DetailView, ListView
+from django.utils import timezone
 
 from blog.models import Post
 
 PER_PAGE = 9
 
 def post_view(request):
-    return render(request, 'blog/post_view.html')
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_view.html', {'posts': posts})
 
