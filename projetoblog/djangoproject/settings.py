920aebc55784b7a221f8a7fa0d6c8a31d58b04f7
mrodriguesoliv <168552816+mrodriguesoliv@users.noreply.github.com>
2024-05-20 02:09:24 +0000

    19.05 23:09

--- projetoblog/djangoproject/settings.py
+++ projetoblog/djangoproject/settings.py
@@ -117,13 +117,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_URL = '/static/'
 
-MEDIA_URL = '/uploads/'  
-  
-# Path where media is stored  
-MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads/')  
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, '/static/images'),
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
