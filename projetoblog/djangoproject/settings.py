9256f13b1b0227e683a93d0e38403d6f7aa65c33
Matheus <mrodriguesoliv@hotmail.com>
2024-05-23 22:50:43 -0300

    23.05 22:50

--- projetoblog/djangoproject/settings.py
+++ projetoblog/djangoproject/settings.py
@@ -117,13 +117,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, '/static/images'),
-]
+MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads/')
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_URL = '/uploads/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
