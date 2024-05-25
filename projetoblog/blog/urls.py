f932c129b684d5c7dc44ae65470722071b946dc7
Matheus <mrodriguesoliv@hotmail.com>
2024-05-25 16:00:40 -0300

    25.05 16:00

--- projetoblog/blog/urls.py
+++ projetoblog/blog/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from blog.views import (post_view)
 from . import views
 
 app_name = 'blog'
 
 urlpatterns = [
     path('', views.post_view, name='post_view'),
+    path('post/<int:pk>/', views.testess, name='testess'),
 ]
--- projetoblog/blog/views.py
+++ projetoblog/blog/views.py
@@ -2,14 +2,18 @@ from typing import Any, Dict
 from django.contrib.auth.models import User
 from django.db.models import Q
 from django.db.models.query import QuerySet
-from django.shortcuts import render
 from django.views.generic import DetailView, ListView
 from django.utils import timezone
 from blog.models import Post
+from django.shortcuts import render, get_object_or_404
 
 PER_PAGE = 9
 
 def post_view(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.order_by('published_date')
     return render(request, 'blog/post_view.html', {'posts': posts})
 
+
+def testess(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/testess.html', {'post': post})
